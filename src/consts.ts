ba085540bdce3fcc0597b8261b1db07b344487cd
Anastasiia Berest <anastasiiaberest@gmail.com>
2026-05-12 11:05:59 +0300

    template

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const SITE_TITLE = 'Zeepay';
+export const SITE_DESCRIPTION = 'Early pay, Automatic savings, Transform your money habits';
+
+
+import LogoIcon from "/img/logo-icon.svg?url";
+export const LOGO_ICON = LogoIcon;
+
+export const NAV_MENU = [
+    { name: "Home", href: "/" },
+    { name: "Pricing", href: "/pricing" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact", href: "/contact" },
+];
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,42 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+  loader: glob({
+    base: "./src/content/blog",
+    pattern: "**/*.{md,mdx}",
+  }),
+
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      category: z.string(),
+    }),
 });
 
-export const collections = { blog };
+const videoLibrary = defineCollection({
+  loader: glob({
+    base: "./src/content/video-library",
+    pattern: "**/*.{md,mdx}",
+  }),
+
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      watch: z.number(),
+    }),
+});
+
+export const collections = {
+  blog,
+  videoLibrary,
+};
